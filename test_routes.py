f9f07beb14b64ac17f177b5b95c6b9cd471b09ce
ShyamSandilya26 <147423773+ShyamSandilya26@users.noreply.github.com>
2023-10-25 14:55:55 +0530

    test_routes.py

---
+++ test_routes.py
@@ -0,0 +1,9 @@
+# tests/test_routes.py
+
+def test_read_product():
+    # Implement the test for reading a product using routes
+
+def test_update_product():
+    # Implement the test for updating a product using routes
+
+# Repeat for other functions
