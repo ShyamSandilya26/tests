721d27baa10518dfa775c3ac50ea74982e851e93
ShyamSandilya26 <147423773+ShyamSandilya26@users.noreply.github.com>
2023-10-25 14:54:54 +0530

    models.py

---
+++ models.py
@@ -0,0 +1,9 @@
+# tests/test_models.py
+
+def test_read_product():
+    # Implement the test for reading a product
+
+def test_update_product():
+    # Implement the test for updating a product
+
+# Repeat for other functions
